0859e391ded5c2d6849ff561affe79cdde1d1fcb
IBogDee <ibogdee@gmail.com>
2022-08-20 15:03:32 +0300

    v1

--- First Run/Assets/Scripts/PlayeCollision.cs
+++ First Run/Assets/Scripts/PlayeCollision.cs
@@ -5,7 +5,8 @@ public class PlayeCollision : MonoBehaviour
 {
 
     public NewMove movement;
- 
+    public Game_manager gameManger;
+
      void OnCollisionEnter(Collision collectionInfo)
     {
         if(collectionInfo.collider.tag == "Obstacle")
--- First Run/Assets/Scripts/Score.cs
+++ First Run/Assets/Scripts/Score.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    public Transform player;
+    public Transform Player;
     public Text scoreText;
     
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = player.position.z.ToString("0");
+        scoreText.text = Player.position.z.ToString("0");
         
     }
 }
